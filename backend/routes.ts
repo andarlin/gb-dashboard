3aab1af256a3f28d6d37f6f987fa2c7210eed31b
Kiko Carisse <kiko@mediabot.ca>
2024-02-08 21:56:31 -0700

    Fix Cors Issue

--- backend/routes.ts
+++ backend/routes.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import bodyParser from "body-parser";
 import studentsController from "./controllers/studentsController";
+import cors from "cors";
 
 const routes = () => {
   const app = express();
   const port = 3000;
 
+  app.use(cors({ origin: ["http://localhost:5500", "http://127.0.0.1:5500"] }));
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(bodyParser.json());
   app.use(bodyParser.raw());
